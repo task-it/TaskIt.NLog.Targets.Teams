0b993961f2c23cb6a7910795e54928090d307077
e.kolios@mellongroup.com <e.kolios@mellongroup.com>
2021-05-18 13:53:19 +0300

    Add proxy support

--- NLog.Targets.Teams/MsTeamsTarget.cs
+++ NLog.Targets.Teams/MsTeamsTarget.cs
@@ -2,6 +2,7 @@
 using NLog.Layouts;
 using NLog.Targets.Teams;
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -45,6 +46,30 @@ namespace NLog.Targets.MsTeams
         /// if ommitted, the Default Implementation in this assembly will be used.
         /// </summary>        
         public string CardAssembly { get; set; } = typeof(DefaultCard).Assembly.GetName().Name;
+
+        /// <summary>
+        /// Proxy to be used<br/>
+        /// Will be displayed as Title in the default card layout
+        /// </summary>
+        public Layout ProxyUrl { get; set; }
+
+        /// <summary>
+        /// Proxy user<br/>
+        /// Used only if ProxyUrl is provided
+        /// </summary>
+        public Layout ProxyUser { get; set; }
+
+        /// <summary>
+        /// Proxy pass<br/>
+        /// Used only if ProxyUrl is provided
+        /// </summary>
+        public Layout ProxyPassword { get; set; }
+
+        /// <summary>
+        /// Proxy bypass on local ip<br/>
+        /// Used only if ProxyUrl is provided
+        /// </summary>
+        public Layout ProxyBypassOnLocal { get; set; }
 
         /// <summary>
         /// Message Card reference
@@ -98,9 +123,13 @@ namespace NLog.Targets.MsTeams
             var applicationName = RenderLogEvent(ApplicationName, logEvent);
             var environment = RenderLogEvent(Environment, logEvent);
             var urlAddress = RenderLogEvent(Url, logEvent);
+            var proxyUrl = RenderLogEvent(ProxyUrl, logEvent);
+            var proxyUser = RenderLogEvent(ProxyUser, logEvent);
+            var proxyPassword = RenderLogEvent(ProxyPassword, logEvent);
+            var proxyBypassOnLocal = RenderLogEvent(ProxyBypassOnLocal, logEvent).Equals("true");
 
             string logMessage = MessageCard.CreateMessage(logEvent, applicationName, environment);
-            var response = await SendMessage(urlAddress, logMessage).ConfigureAwait(false);
+            var response = await SendMessage(urlAddress, proxyUrl, proxyUser, proxyPassword, proxyBypassOnLocal, logMessage).ConfigureAwait(false);
             if (!response.IsSuccessStatusCode)
             {
                 throw new InvalidOperationException($"Rest Call Failed - {response.ReasonPhrase}");
@@ -110,12 +139,19 @@ namespace NLog.Targets.MsTeams
         /// <summary>
         /// posts the message to the url
         /// </summary>
-        private async Task<HttpResponseMessage> SendMessage(string urlAddress, string logMessage)
+        private async Task<HttpResponseMessage> SendMessage(string urlAddress, string proxyUrl, string proxyUser, string ProxyPass, bool proxyBypassOnLocal, string logMessage)
         {
             var messageContent = new StringContent(logMessage);
             messageContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            var proxiedHttpClientHandler = new HttpClientHandler();
 
-            using (var httpClient = new HttpClient())
+            if (!string.IsNullOrEmpty(proxyUrl))
+            {
+                proxiedHttpClientHandler = new HttpClientHandler() { UseProxy = true };
+                proxiedHttpClientHandler.Proxy = new WebProxy(proxyUrl);
+            }
+
+            using (var httpClient = new HttpClient(proxiedHttpClientHandler))
             {
                 var targetUrl = new Uri(urlAddress);
                 return await httpClient.PostAsync(targetUrl, messageContent).ConfigureAwait(false);
